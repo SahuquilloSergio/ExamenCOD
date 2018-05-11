17f020db3d00130d5ae7a3b9454892eb6d1474c5
ssahuquilloembade <ssahuquilloembade@victoria10.danielcastelao.org>
2018-05-11 10:04:06 +0200

    FIN

--- src/org/sergio/examencod/BotonAction.java
+++ src/org/sergio/examencod/BotonAction.java
@@ -7,9 +7,14 @@ package org.sergio.examencod;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.swing.JOptionPane;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
+import org.openide.util.Exceptions;
 import org.openide.util.NbBundle.Messages;
 
 @ActionID(
@@ -27,6 +32,44 @@ public final class BotonAction implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO implement action body
+    
+        String categoria=JOptionPane.showInputDialog("Bcategory: (Education)");
+        String outdir=JOptionPane.showInputDialog("outdir: ");
+        String outfile=JOptionPane.showInputDialog("outfile: ");
+        String srcdir=JOptionPane.showInputDialog("ruta del archivo");
+        String srcfiles=JOptionPane.showInputDialog("nombre del archivo.jar");
+        String appclass=JOptionPane.showInputDialog("paquete+mainclass");
+        String title=JOptionPane.showInputDialog("descripcion");
+                
+        try {
+            
+            String cmd="javapackager -deploy -native deb -Bcategory="+categoria+
+                    " -outdir "+outdir+" -outfile "+outfile+" -srcdir "+srcdir+" -srcfiles "+srcfiles+
+                    " -appclass "+appclass+" -title "+title;
+            
+            Runtime rt = Runtime.getRuntime();
+            //Process pr = rt.exec("cmd /c dir");
+            Process pr = rt.exec(cmd);
+            
+            BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+            
+            String line=null;
+            
+            while((line=input.readLine()) != null) {
+                System.out.println(line);
+            }
+            
+            int exitVal = pr.waitFor();
+            System.out.println("Exited with error code "+exitVal);
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        } catch (InterruptedException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+ 
+            
+
     }
-}
+    }
+
+
